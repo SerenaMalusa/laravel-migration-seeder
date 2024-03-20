34964d6c8550739bb82077fffbea6c3869891c06
SerenaMalusa <sere.malu@gmail.com>
2024-03-20 18:17:50 +0100

    feat: create view for today's trains

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\train;
 use App\Models\Train as ModelsTrain;
 use Illuminate\Http\Request;
+use Illuminate\Support\Composer;
 
 class PageController extends Controller
 {
     public function index()
     {
-        $trains = ModelsTrain::all();
-        // return view('pages.home');
-        dd($trains);
+        $today_trains = ModelsTrain::whereDate('departure_time', '=', date('Y-m-d'))->get();
+        if (empty($today_trains)) abort(404);
+        // $trains = ModelsTrain::all();
+        return view('pages.home', compact('today_trains'));
+        // dd($today_trains);
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,5 +3,35 @@
 @section('title', 'home')
 
 @section('main-content')
-  <h1>home</h1>
+  <h1 class="mb-5">Trains departed/departing today: {{ date('Y-m-d') }}</h1>
+  <div class="row g-3 mt-2">
+    @forelse ($today_trains as $train)
+      <div class="col-6">
+        <div @class([
+          'card',
+          'h-100',
+          "text-decoration-line-through" => $train->deleted
+        ])>
+
+          <div class="card-body">
+            <h5 class="card-title">
+              From {{ $train->departure_station }} to {{ $train->arrival_station }}
+            </h5>
+            <ul class="list-group list-group-flush m-0 p-0">
+              <li class="list-group-item">Departure at {{ $train->departure_time }} - Arrival at {{ $train->arrival_time }}</li>
+              <li class="list-group-item">Train n: {{ $train->train_code }}</li>
+              <li class="list-group-item">{{ $train->coaches }} coaches</li>
+              @if(!$train->on_time)
+              <li class="list-group-item">Delay</li>
+              @endif
+            </ul>
+          
+          </div>
+        
+        </div>
+      </div>
+    @empty
+        <li>Sorry, no trains are departing today</li>
+    @endforelse
+    </div>
 @endsection
